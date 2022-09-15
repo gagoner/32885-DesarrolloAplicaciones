982b8d9e7f1d6e02ad08a16ed8e9c534603acdbe
gagoner <gagoner@gmail.com>
2022-09-15 00:19:02 -0300

    Desafío 2 actualizado según tutorial

--- 02.-Lista_optimizada/App.js
+++ 02.-Lista_optimizada/App.js
@@ -1,55 +1,37 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity, Keyboard } from "react-native";
-import Task from "./components/task";
+import React, {useState} from 'react';
+import { Keyboard, ScrollView, StyleSheet, Text, View } from 'react-native';
+import TaskInputField from './components/TaskInputField';
+import TaskItem from './components/TaskItem';
 
 export default function App() {
-  const [task, setTask] = useState();
-  const [taskItems, setTaskItems] = useState([]);
 
-  const handleAddTask = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    if (task == null) return;
+    setTasks([...tasks, task]);
     Keyboard.dismiss();
-    setTaskItems([...taskItems, task]);
-    setTask(null);
-  };
-  const handleDeleteTask = (index) => {
-    let itemsCopy = [...taskItems];
-    itemsCopy.splice(index, 1);
-    setTaskItems(itemsCopy);
-  };
+  }
+
+  const deleteTask = (deleteIndex) => {
+    setTasks(tasks.filter((value, index) => index != deleteIndex));
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.taskWrapper}>
-        <Text style={styles.sectionTitle}>Today's task</Text>
-        <View style={styles.items}>
-          {taskItems.map((item, index) => {
-            return (
-              <TouchableOpacity
-                key={index}
-                onPress={() => handleDeleteTask(index)}
-              >
-                <Task text={item} />
-              </TouchableOpacity>
-            );
-          })}
-        </View>
-      </View>
-      <KeyboardAvoidingView
-        behavor={Platform.OS === "ios" ? "padding" : "height"}
-        style={styles.writeTaskWrapper}
-      >
-        <TextInput
-          style={styles.input}
-          placeholder={"Write your task"}
-          value={task}
-          onChangeText={(text) => setTask(text)}
-        />
-        <TouchableOpacity onPress={() => handleAddTask()}>
-          <View style={styles.addWrapper}>
-            <Text style={styles.addText}>+</Text>
-          </View>
-        </TouchableOpacity>
-      </KeyboardAvoidingView>
+        <Text style={styles.heading}>TODO LIST</Text>
+      <ScrollView style={styles.scrollView}>
+        {
+        tasks.map((task, index) => {
+          return (
+            <View key={index} style={styles.taskContainer}>
+              <TaskItem index={index + 1} task={task} deleteTask={() => deleteTask(index)}/>
+            </View>
+          );
+        })
+      }
+      </ScrollView>
+      <TaskInputField addTask={addTask}/>
     </View>
   );
 }
@@ -57,45 +39,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#E8EAED",
+    backgroundColor: '#1E1A3C',
   },
-  taskWrapper: {
-    paddingTop: 80,
-    paddingHorizontal: 20,
+  heading: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: '600',
+    marginTop: 80,
+    marginBottom: 10,
+    marginLeft: 20,
+    textAlign: "center",
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
+  scrollView: {
+    marginBottom: 70,
   },
-  items: {
-    marginTop: 30,
-  },
-  writeTaskWrapper: {
-    position: "absolute",
-    bottom: 60,
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
-  },
-  input: {
-    paddingVertical: 15,
-    paddingHorizontal: 15,
-    width: 250,
-    backgroundColor: "#fff",
-    borderRadius: 60,
-    borderColor: "#c0c0c0",
-    borderWidth: 1,
-  },
-  addWrapper: {
-    width: 60,
-    height: 60,
-    backgroundColor: "#fff",
-    borderRadius: 60,
-    justifyContent: "center",
-    alignItems: "center",
-    borderColor: "#c0c0c0",
-    borderWidth: 1,
-  },
-  addText: {},
+  taskContainer: {
+    marginTop: 20,
+  }
 });
